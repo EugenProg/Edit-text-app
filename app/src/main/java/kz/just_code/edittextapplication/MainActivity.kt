5aefc9abe7b8c92cbbd7cbaf1e5b4e9e19dc0e34
genius <Evko90@Yandex.ru>
2023-10-19 23:23:11 +0600

    Add spannable

--- app/src/main/java/kz/just_code/edittextapplication/MainActivity.kt
+++ app/src/main/java/kz/just_code/edittextapplication/MainActivity.kt
@@ -1,9 +1,20 @@
 package kz.just_code.edittextapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.Typeface
 import android.os.Bundle
-import android.telephony.PhoneNumberUtils
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.text.style.UnderlineSpan
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import kz.just_code.edittextapplication.databinding.ActivityMainBinding
@@ -12,7 +23,6 @@ import ru.tinkoff.decoro.MaskImpl
 import ru.tinkoff.decoro.slots.PredefinedSlots
 import ru.tinkoff.decoro.slots.Slot
 import ru.tinkoff.decoro.watchers.MaskFormatWatcher
-import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -49,6 +59,48 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.editText.addTextChangedListener()
+        binding.cityView.movementMethod = LinkMovementMethod.getInstance()
+        binding.cityView.text = getCity()
+    }
+
+    private fun getCity(): SpannableString {
+        val cityName = "Netherlands, Amesterdam"
+        val spannable = SpannableString("$cityName ${getString(R.string.change_location)}")
+
+        val color = Color.parseColor("#8E63EE")
+        spannable.setSpan(ForegroundColorSpan(color), cityName.length + 1, spannable.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+        spannable.setSpan(UnderlineSpan(), cityName.length + 1, spannable.length, Spannable.SPAN_INCLUSIVE_EXCLUSIVE)
+        spannable.setSpan(
+            StyleSpan(Typeface.BOLD),
+            cityName.length + 1,
+            spannable.length,
+            Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+        spannable.setSpan(
+            StyleSpan(Typeface.ITALIC),
+            0,
+            cityName.length,
+            Spannable.SPAN_INCLUSIVE_EXCLUSIVE
+        )
+
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                Toast.makeText(this@MainActivity, "Click at location", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                ds.isUnderlineText = true
+                ds.linkColor = Color.parseColor("#8E63EE")
+            }
+        }
+
+        spannable.setSpan(
+            clickableSpan,
+            cityName.length + 1,
+            spannable.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        return spannable
     }
 
     override fun onResume() {
